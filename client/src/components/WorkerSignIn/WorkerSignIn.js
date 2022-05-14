220fd802cdb02f169ade7b918d60a885643cd741
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-05-14 09:40:42 +0530

    WorkerSignIn

--- client/src/components/WorkerSignIn/WorkerSignIn.js
+++ client/src/components/WorkerSignIn/WorkerSignIn.js
@@ -1,9 +1,116 @@
-import React from 'react'
+import React from 'react';
+
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
+import { withStyles } from "@mui/styles";
+
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+
+import signup from '../../assets/workersign.jpg';
+
+import './WorkerSignIn.css';
+
+const theme = createTheme();
+
+const CssTextField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "black"
+    },
+
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "blue"
+      },
+      "&:hover fieldset": {
+        borderColor: "black"
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "blue"
+      }
+    }
+  }
+})(TextField);
 
 const WorkerSignIn = () => {
+
+  const mediaLessthanmd = useMediaQuery(theme.breakpoints.down("md"));
+  
+
   return (
     <>
-      WorkerSignIn
+      <Navbar />
+        <div className="signup-container">
+          <div className="signup-content">
+            <h1>Sign As Worker</h1>
+          </div>        
+
+          <ThemeProvider theme={theme}>
+            <Container component="main">
+              <CssBaseline />
+
+              <form style={{ marginTop: "5vh" }}>
+                <div className="registrationForm" style={{ display: "flex" }}>
+                  {!mediaLessthanmd && (
+                    <Grid container style={{ flex: "6" }}>
+                      <img src={signup} alt=""  />
+                    </Grid>
+                  )}
+                  <div className="registrationFormContainer" style={{ flex: "6" }}>                    
+
+                    <Grid container spacing={2}  >
+                      <Grid item md={12} xs={8}>
+                        <CssTextField label="Username" variant="outlined" fullWidth required autoFocus
+                          InputProps={{ style: {color: "black"}}}
+                          type="text"
+                        />
+                      </Grid>
+
+                      <Grid item md={12} xs={8}>
+                        <CssTextField label="Email" variant="outlined" fullWidth required autoFocus
+                          InputProps={{ style: {color: "black"}}}
+                          type="email"
+                        />
+                      </Grid>
+
+                      <Grid item md={12} xs={8}>
+                        <CssTextField label="Password" variant="outlined" fullWidth required autoFocus
+                          InputProps={{ style: {color: "black"}}}
+                          type="password"
+                        />
+                      </Grid>                      
+                      
+                      <Grid item md={12} xs={8}>
+                        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} >
+                          Sign In
+                        </Button>
+                      </Grid>
+
+                    </Grid>
+
+                    <Grid container justifyContent="center">
+                      <Grid item>
+                        <Link href="/shopjoin" variant="body2">
+                          Don't have an account? Sign up
+                        </Link>
+                      </Grid>
+                    </Grid>
+
+                  </div>
+                </div>
+              </form>
+            </Container>
+          </ThemeProvider>  
+
+        </div>  
+      <Footer />
     </>
   )
 }
