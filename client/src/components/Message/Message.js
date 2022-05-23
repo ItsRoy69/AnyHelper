980dd69081d532c3ea84be564a3dc85a9ffbc08e
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-05-23 20:31:13 +0530

    Message Box done

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import user from "./../../assets/user.webp";
+
+import { ImSearch } from "react-icons/im";
+import { IoMdSend } from "react-icons/io";
 
 import './Message.css';
 
@@ -10,7 +14,105 @@ const Message = () => {
     <>
       <Navbar />
         <div className="container">
-          
+          <div className='messagebox'>            
+            <div className='messagenamebox'>
+              <div className='messagenamesearchbox'>
+                <div className="search-box">
+                    <input
+                      type="text"
+                      id=""
+                      className="search"
+                    />
+                </div>
+                <div className="searchbtn">
+                    <ImSearch />                  
+                </div>
+              </div>
+              <div className="name-box">              
+                <div
+                  className="names"
+                >
+                  <img src={user} alt="..." />
+                  <h3>space.chatHead</h3>
+                </div>
+                <div
+                  className="names"
+                >
+                  <img src={user} alt="..." />
+                  <h3>space.chatHead</h3>
+                </div>
+                <div
+                  className="names"
+                >
+                  <img src={user} alt="..." />
+                  <h3>space.chatHead</h3>
+                </div>
+                <div
+                  className="names"
+                >
+                  <img src={user} alt="..." />
+                  <h3>space.chatHead</h3>
+                </div>
+                <div
+                  className="names"
+                >
+                  <img src={user} alt="..." />
+                  <h3>space.chatHead</h3>
+                </div>
+                <div
+                  className="names"
+                >
+                  <img src={user} alt="..." />
+                  <h3>space.chatHead</h3>
+                </div>
+                <div
+                  className="names"
+                >
+                  <img src={user} alt="..." />
+                  <h3>space.chatHead</h3>
+                </div>
+              </div>
+            </div>
+            <div className='messagechatbox'>
+              <div className="message">
+                <div className="message-content">
+                  <p>messages.message</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">2:30</p>
+                  <p id="author">messages.username</p>
+                </div>                
+              </div>
+              <div className="message">
+                <div className="message-content">
+                  <p>messages.message</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">2:30</p>
+                  <p id="author">messages.username</p>
+                </div>                
+              </div>
+              <div className="message">
+                <div className="message-content">
+                  <p>messages.message</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">2:30</p>
+                  <p id="author">messages.username</p>
+                </div>                
+              </div>
+            </div>
+            <div className="chat-section">
+              <input
+                type="text"
+                placeholder="Hey..."
+                className='chat-input'
+              />
+              <button>
+                <IoMdSend />
+              </button>
+            </div>
+          </div>
         </div>
       <Footer />
     </>
