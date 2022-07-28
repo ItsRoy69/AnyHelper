9e0765ec9551aeb2a6956ab711f693418f3eeb3c
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-07-28 08:45:46 +0530

    upgrade

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home/Home";
-import ElectricalShops from "./pages/ElectricalShops";
-import MechanicShops from "./pages/MechanicShops";
-import PlumberShop from "./pages/PlumberShops";
-import Workers from "./pages/Workers";
-import Items from "./pages/Items";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import ShopJoin from "./pages/ShopJoin";
-import ShopSignIn from "./pages/ShopSignIn";
-import WorkerJoin from "./pages/WorkerJoin";
-import WorkerSignIn from "./pages/WorkerSignIn";
-import Messages from './pages/Messages';
 
 function App() {
   return (
-    <div className="App"> 
-      <Router> 
-        <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/electricalshops" exact element={<ElectricalShops />} />
-            <Route path="/mechanicshops" exact element={<MechanicShops />} />
-            <Route path="/plumbershops" exact element={<PlumberShop />}  />
-            <Route path="/workers" exact element={<Workers />} />
-            <Route path="/items" exact element={<Items />} />
-            <Route path="/signup" exact element={<SignUp />} />
-            <Route path="/signin" exact element={<SignIn />} />
-            <Route path="/shopjoin" exact element={<ShopJoin />} />
-            <Route path="/shopsignin" exact element={<ShopSignIn />} />
-            <Route path="/workerjoin" exact element={<WorkerJoin />} />
-            <Route path="/workersignin" exact element={<WorkerSignIn />} />
-            <Route path="/messages" exact element={<Messages />} />
-        </Routes> 
-      </Router>     
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
