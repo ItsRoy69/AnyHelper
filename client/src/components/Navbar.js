73f44d4a1b45315575caba91dda8851674504445
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-07-28 11:21:37 +0530

    Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import "../styles/Navbar.css";
+
+import { MdExpandMore } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar fixed-top navbar-expand-lg ">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            ANYHELPER
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            {/* <span className="navbar-toggler-icon">-</span> */}
+            <MdExpandMore />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Blogs
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#services">
+                  Services
+                </a>
+              </li>              
+              <li className="nav-item">
+                <a className="nav-link" href="#team">
+                  Register as Professional
+                </a>
+              </li>
+              <Link to="sign-up" className="nav-btnlink">
+                <button className="btn">Sign Up</button>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from "../components/Navbar";
+import "../styles/Home.css"
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Home
