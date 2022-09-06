e903d38354ca1dd85f9799ae486ad62b6aecede8
Tamal Das <gyansujan69@gmail.com>
2022-09-06 21:39:57 +0530

    Tamal debugs (#8)
    * fix : added modal for edit user section
    
     - http://localhost:3000/userdashboard
    
    * fix  : add items base-design
    
    * feat : Add items modal for shops

---
+++ client/src/components/AddItems.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AddItems = () => {
+
+
+
+
+
+
+    return (
+        <>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <input
+                    type="text"
+                    placeholder="Enter item name"
+                    name='item'
+                    className="infoInput"
+
+                />
+            </div>
+
+        </>
+    )
+}
+
+export default AddItems
--- client/src/components/EditUserDashboard.js
+++ client/src/components/EditUserDashboard.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
 import "../styles/EditUserDashboard.css";
 
-const EditUserDashboard = ({  setModal }) => {
-
-
-  return (
-    <>
-      <div className='EditDashboard'onClose={()=>setModal(false)}>
+const EditUserDashboard = ({ setModal }) => {
+    return (
+        <>
+            {/* <div className='EditDashboard'onClose={()=>setModal(false)}>
         <div className='editDashboardbody'>
             <form className="infoForm">
                 <h3>Your Info</h3>
@@ -53,10 +51,50 @@ const EditUserDashboard = ({  setModal }) => {
                 </button>
             </form> 
         </div>
-      </div>
+      </div> */}
 
-    </>
-  )
-}
+            <div className="EditDashboard_ParentMain">
+                <div className="EditDashboard_Formdiv1">
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        name="firstname"
+                        className="infoInput"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        name="lastname"
+                        className="infoInput"
+                    />
+                </div>
 
-export default EditUserDashboard
+                <div className="EditDashboard_Formdiv2">
+                    <input
+                        type="text"
+                        placeholder="Lives in"
+                        name="livesIn"
+                        className="infoInput"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Country"
+                        name="country"
+                        className="infoInput"
+                    />
+                </div>
+
+                <div className='editDashboard_image'>
+                    <h6>Profile image</h6>
+                    <input type="file" name="profileImage" />
+                    <h6>Cover image</h6>
+                    <input type="file" name="coverImage" />
+                </div>
+
+
+            </div>
+        </>
+    );
+};
+
+export default EditUserDashboard;
