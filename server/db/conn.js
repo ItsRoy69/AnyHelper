c3a5556da105e22d7b707a3c9d9eac48748620cb
Samrat <samratmukherjee.sps@gmail.com>
2022-09-18 19:45:05 +0530

    finished

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const DB = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.s5miy.mongodb.net/?retryWrites=true&w=majority`;
+// const DB = "mongodb+srv://xyber:xyber1004@xybercluster.usni0.mongodb.net/AnyHelper?retryWrites=true&w=majority";
 
 module.exports = async function connection (){
   try{
